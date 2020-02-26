eff08ea32cfd12e8affb63839d9b767667e7b5b7
gonzaloiniesta <Benicassim1999>
2020-02-26 16:25:40 +0100

    Pueba de funcinamiento

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,5 +1,5 @@
 public class main {
     public static void main(String []args){
-        System.out.println("Treball Diseño 2");
+        System.out.println("Treball Diseño 2 esto va" + "");
     }
 }
