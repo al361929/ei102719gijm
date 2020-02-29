bb0d17f0b57136d29d0c37fbabae813241168602
Joaquin Martí Oria <47143900+al375883@users.noreply.github.com>
2020-02-29 20:20:44 +0100

    Funcional

---
+++ src/main/java/majorsacasa/MajorsACasaApplication.java
@@ -0,0 +1,18 @@
+package majorsacasa;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+
+import java.util.logging.Logger;
+
+@SpringBootApplication
+public class MajorsACasaApplication {
+
+	private static final Logger log = Logger.getLogger(MajorsACasaApplication.class.getName());
+
+	public static void main(String[] args) {
+		// Auto-configura l'aplicació
+		new SpringApplicationBuilder(MajorsACasaApplication.class).run(args);
+		System.out.println("La apicacion funciona dpm");
+	}
+}
