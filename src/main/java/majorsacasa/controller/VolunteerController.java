b657074d54ea1a8d3a3862c5de739979d80fc8b7
al375923 <al375923@uji.es>
2020-03-03 19:46:37 +0100

    problemas con autentficar o eso dice esto, to mentira

--- src/main/java/majorsacasa/controller/VolunteerController.java
+++ src/main/java/majorsacasa/controller/VolunteerController.java
@@ -1,4 +1,23 @@
 package majorsacasa.controller;
 
+import majorsacasa.dao.VolunteerDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/volunteer")
 public class VolunteerController {
+    private VolunteerDao volunteerDao;
+    @Autowired
+    public void setNadadorDao(VolunteerDao volunteerDao) {
+        this.volunteerDao=volunteerDao;
+    }
+    @RequestMapping("/list")
+    public String listVolunteers(Model model) {
+        model.addAttribute("volunteers", volunteerDao.getVolunteers());
+        return "nadador/list";
+    }
+    //----
 }
