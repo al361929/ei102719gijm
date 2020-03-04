63c11451651b0ab62a1e86d82e19732eb0b3905b
al375923 <al375923@uji.es>
2020-03-04 17:06:14 +0100

    volunter rowmaper completo

--- src/main/java/majorsacasa/dao/VolunteerRowMapper.java
+++ src/main/java/majorsacasa/dao/VolunteerRowMapper.java
@@ -14,5 +14,11 @@ public final class VolunteerRowMapper implements RowMapper<Volunteer> {
         volunteer.setApellidos(rs.getString("surname"));
         volunteer.setDireccion(rs.getString("address"));
         volunteer.setDni(rs.getString("dni"));
+        volunteer.setTelefono(rs.getString("phonenumber"));
+        volunteer.setBirthday(rs.getDate("birthday").toLocalDate());
+        volunteer.setDataDown(rs.getDate("dateDown").toLocalDate());
+        volunteer.setRealaseDate(rs.getDate("realasedate").toLocalDate());
+        volunteer.setContraseña(rs.getString("password"));
+        volunteer.setUsuario(rs.getString("user_Name"));
         return volunteer;    }
 }
