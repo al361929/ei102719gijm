261f7c8076b641ea7d97a572eb0ffa23e0f069ab
al375923 <al375923@uji.es>
2020-03-04 15:34:21 +0100

    clase elderly

---
+++ src/main/java/majorsacasa/model/Elderly.java
@@ -0,0 +1,118 @@
+package majorsacasa.model;
+
+import java.time.LocalDate;
+
+public class Elderly {
+    String nombre;
+    String apellidos;
+    String direccion;
+    String dni;
+    String alergias;
+    String telefono;
+    String usuario;
+    String contraseña;
+    LocalDate realaseDate;
+    LocalDate dataDown;
+    LocalDate birthday;
+    String cuentaBancaria;
+
+
+    public Elderly() {
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public String getAlergias() {
+        return alergias;
+    }
+
+    public void setAlergias(String alergias) {
+        this.alergias = alergias;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getContraseña() {
+        return contraseña;
+    }
+
+    public void setContraseña(String contraseña) {
+        this.contraseña = contraseña;
+    }
+
+    public LocalDate getRealaseDate() {
+        return realaseDate;
+    }
+
+    public void setRealaseDate(LocalDate realaseDate) {
+        this.realaseDate = realaseDate;
+    }
+
+    public LocalDate getDataDown() {
+        return dataDown;
+    }
+
+    public void setDataDown(LocalDate dataDown) {
+        this.dataDown = dataDown;
+    }
+
+    public LocalDate getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(LocalDate birthday) {
+        this.birthday = birthday;
+    }
+
+    public String getCuentaBancaria() {
+        return cuentaBancaria;
+    }
+
+    public void setCuentaBancaria(String cuentaBancaria) {
+        this.cuentaBancaria = cuentaBancaria;
+    }
+}
