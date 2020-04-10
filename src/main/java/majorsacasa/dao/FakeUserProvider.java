2d4d441cdde7c5d6101fd4051bb86b9b5f884d15
Philippe <al375923@uji.es>
2020-04-10 14:29:10 +0200

    jaleo con el FakeUserProvider para obtener los datos de los  demas

--- src/main/java/majorsacasa/dao/FakeUserProvider.java
+++ src/main/java/majorsacasa/dao/FakeUserProvider.java
@@ -1,19 +1,46 @@
 package majorsacasa.dao;
 
 import majorsacasa.model.UserDetails;
+import majorsacasa.model.Volunteer;
 import org.jasypt.util.password.BasicPasswordEncryptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.Map;
+import javax.sql.DataSource;
+import java.util.*;
 
 @Repository
 public class FakeUserProvider implements UserDao {
     final Map<String, UserDetails> knownUsers = new HashMap<String, UserDetails>();
+    private JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public void setDataSource(DataSource dataSource) {
+        this.jdbcTemplate = new JdbcTemplate(dataSource);
+    }
+
+    public List<Volunteer> getVolunteers() {
+        try {
+            return jdbcTemplate.query(
+                    "SELECT * FROM Volunteer",
+                    new VolunteerRowMapper());
+        } catch (EmptyResultDataAccessException e) {
+            return new ArrayList<Volunteer>();
+        }
+    }
 
     public FakeUserProvider() {
+        List<Volunteer> list = getVolunteers();
         BasicPasswordEncryptor passwordEncryptor = new BasicPasswordEncryptor();
+        for (Volunteer v : list) {
+            UserDetails user = new UserDetails();
+            user.setUsername(v.getUsuario());
+            user.setPassword(passwordEncryptor.encryptPassword(v.getContraseña()));
+            knownUsers.put(v.getUsuario(), user);
+
+        }
         UserDetails userAlice = new UserDetails();
         userAlice.setUsername("alice");
         userAlice.setPassword(passwordEncryptor.encryptPassword("alice"));
@@ -27,6 +54,7 @@ public class FakeUserProvider implements UserDao {
 
     @Override
     public UserDetails loadUserByUsername(String username, String password) {
+        System.out.println(username + "---" + password);
         UserDetails user = knownUsers.get(username.trim());
         if (user == null)
             return null; // Usuari no trobat
---
+++ src/main/java/majorsacasa/dao/UserRowMapper.java
@@ -0,0 +1,22 @@
+package majorsacasa.dao;
+
+import majorsacasa.model.UserDetails;
+import majorsacasa.model.Volunteer;
+import org.springframework.jdbc.core.RowMapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.time.LocalDate;
+
+public final class UserRowMapper implements RowMapper<UserDetails> {
+    @Override
+    public UserDetails mapRow(ResultSet rs, int i) throws SQLException {
+        UserDetails user = new UserDetails();
+        user.setUsername(rs.getString("username"));
+        user.setPassword(rs.getString("password"));
+        return user;
+    }
+}
+
+
+
