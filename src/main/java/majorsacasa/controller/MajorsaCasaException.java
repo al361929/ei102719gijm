ccc8e118aadd31acbbdcfae9e699a84835cf30da
al375923 <al375923@uji.es>
2020-04-12 17:47:48 +0200

    tratando errores comunes

---
+++ src/main/java/majorsacasa/controller/MajorsaCasaException.java
@@ -0,0 +1,31 @@
+package majorsacasa.controller;
+
+public class MajorsaCasaException extends RuntimeException {
+
+    String message;    // Missatge per mostrar a la vista
+    String errorName;     // Identificador de l’error
+
+    public MajorsaCasaException(String message, String errorName)
+    {
+        this.message=message;
+        this.errorName=errorName;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getErrorName() {
+        return errorName;
+    }
+
+    public void setErrorName(String errorName) {
+        this.errorName = errorName;
+    }
+
+
+}
