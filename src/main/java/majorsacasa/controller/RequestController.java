0609adc2b3fa98a6b6107b5a0abf2bd31a94a2d7
Philippe <al375923@uji.es>
2020-04-16 17:40:50 +0200

    todos los errores coregidos de momento

--- src/main/java/majorsacasa/controller/RequestController.java
+++ src/main/java/majorsacasa/controller/RequestController.java
@@ -2,22 +2,24 @@ package majorsacasa.controller;
 
 import majorsacasa.dao.RequestDao;
 import majorsacasa.model.Request;
+import majorsacasa.model.UserDetails;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpSession;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
 @Controller
 @RequestMapping("/request")
-public class RequestController {
+public class RequestController extends Controlador{
 
     private RequestDao requestDao;
-    private List estados = Arrays.asList("Pendiente", "Aceptada", "Rechazada");
+    private List estados = Arrays.asList("Pendiente", "Aceptada", "Rechazada","Cancelada");
     @Autowired
     public void setRequestDao(RequestDao requestDao) {
         this.requestDao = requestDao;
@@ -67,4 +69,35 @@ public class RequestController {
         requestDao.deleteRequest(id);
         return "redirect:../list";
     }
+    @RequestMapping("/listElderly")
+    public String listRequestsElderly(HttpSession session,Model model, @RequestParam("nuevo") Optional<String> nuevo) {
+        UserDetails user = (UserDetails) session.getAttribute("user");
+
+        model.addAttribute("requests", requestDao.getRequestsElderly(user.getDni()));
+        String newVolunteerTime = nuevo.orElse("None");
+        model.addAttribute("nuevo", newVolunteerTime);
+        return gestionarAcceso(session,model,"ElderlyPeople","request/listRequestElderly");
+
+    }
+    @RequestMapping(value = "/addRequestElderly")
+    public String addRequestElderly(Model model) {
+        model.addAttribute("request", new Request());
+        return "request/addRequestElderly";
+    }
+
+    @RequestMapping(value = "/addRequestElderly", method = RequestMethod.POST)
+    public String processAddSubmitRequestElderly(HttpSession session,@ModelAttribute("request") Request request, BindingResult bindingResult) {
+        if (bindingResult.hasErrors())
+            return "request/addRequestElderly";
+        UserDetails user = (UserDetails) session.getAttribute("user");
+        request.setDni(user.getDni());
+        requestDao.addRequest(request);
+        return "redirect:listElderly?nuevo=" + request.getIdRequest();
+    }
+    @RequestMapping(value = "/cancelarRequest/{id}")
+    public String processUpdateEstadp(@PathVariable int id) {
+        requestDao.updateEstado(id,"Cancelada");
+        return "redirect:../listElderly";
+    }
+
 }
